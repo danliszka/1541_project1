8614ddd72a27592a226f905a7877959ef58c3b9d
danliszka <danliszka1@gmail.com>
2018-02-18 15:19:11 -0500

    integrated will's code

--- cpu.c
+++ cpu.c
@@ -13,6 +13,10 @@
 
 #define HASHSIZE 64
 
+//Function prototype
+int hazardCheck(struct trace_item *a, struct trace_item *b, struct trace_item *c, struct trace_item *d, struct trace_item *e, struct trace_item *f, struct trace_item *g);
+
+
 int main(int argc, char **argv)
 {
   struct trace_item *tr_entry;
@@ -110,10 +114,6 @@ int main(int argc, char **argv)
   while(1) {
     foundControlHazard = 0;
 
-    if (hazardType == 0 || hazardType == 4)
-    {
-      size = trace_get_item(&tr_entry);
-    }
     if (!size && remaining == 0)
     {
       /* no more instructions (trace_items) to simulate */
@@ -142,6 +142,8 @@ int main(int argc, char **argv)
           // No prediction
           if (prediction_type == 0 && EX->Addr != ID->PC) 
           {
+            size = trace_get_item(&tr_entry);
+
             WB = MEM2;
             MEM2 = MEM1;
             MEM1 = EX;
@@ -166,6 +168,8 @@ int main(int argc, char **argv)
                 hashTable[hashIndex] = 1;
 
                 //squash instructions
+                size = trace_get_item(&tr_entry);
+
                 WB = MEM2;
                 MEM2 = MEM1;
                 MEM1 = EX;
@@ -196,6 +200,8 @@ int main(int argc, char **argv)
                 hashTable[hashIndex] = 0; 
 
                 //squash instructions
+                size = trace_get_item(&tr_entry);
+
                 WB = MEM2;
                 MEM2 = MEM1;
                 MEM1 = EX;
@@ -222,6 +228,8 @@ int main(int argc, char **argv)
                 hashTable[hashIndex] = 1;
 
                 //squash instructions
+                size = trace_get_item(&tr_entry);
+
                 WB = MEM2;
                 MEM2 = MEM1;
                 MEM1 = EX;
@@ -249,6 +257,8 @@ int main(int argc, char **argv)
                   hashTable[hashIndex] = 1;
 
                   //squash instructions
+                  size = trace_get_item(&tr_entry);
+
                   WB = MEM2;
                   MEM2 = MEM1;
                   MEM1 = EX;
@@ -273,6 +283,8 @@ int main(int argc, char **argv)
                   hashTable[hashIndex] = 3;
 
                   //squash instructions
+                  size = trace_get_item(&tr_entry);
+
                   WB = MEM2;
                   MEM2 = MEM1;
                   MEM1 = EX;
@@ -302,6 +314,8 @@ int main(int argc, char **argv)
                   hashTable[hashIndex] = 0;
 
                   //squash instructions
+                  size = trace_get_item(&tr_entry);
+
                   WB = MEM2;
                   MEM2 = MEM1;
                   MEM1 = EX;
@@ -327,6 +341,8 @@ int main(int argc, char **argv)
                   hashTable[hashIndex] = 2;
 
                   //squash instructions
+                  size = trace_get_item(&tr_entry);
+
                   WB = MEM2;
                   MEM2 = MEM1;
                   MEM1 = EX;
@@ -347,11 +363,13 @@ int main(int argc, char **argv)
         // If a cycle did not already execute to fix a control hazard
         if (!foundControlHazard)
         {
+
         //Now check for structural or data hazard
-        //INSERT WILL's FUNCTION HERE!!!!!
+        hazardType = hazardCheck(IF1, IF2, ID, EX, MEM1, MEM2, WB);
 
           switch (hazardType) {
           case 0: //No hazard, normal cycle
+            size = trace_get_item(&tr_entry);
             WB = MEM2;
             MEM2 = MEM1;
             MEM1 = EX;
@@ -408,7 +426,7 @@ int main(int argc, char **argv)
           printf("[cycle %d] SQUASHED\n",cycle_number);
           break;
         case ti_NOP:
-          printf("[cycle %d] NOP\n:",cycle_number) ;
+          printf("[cycle %d] NOP\n",cycle_number) ;
           break;
         case ti_RTYPE:
           printf("[cycle %d] RTYPE:",cycle_number) ;
@@ -451,3 +469,42 @@ int main(int argc, char **argv)
 }
 
 
+
+
+//retuns an integer based on the hazard
+//1 - Structural Hazard, stall IF1, IF2, and ID
+//2 - Data Hazard a) stall ID and EX, insert NO-OP into MEM1
+//3 - Data Hazard b) stall ID and EX, insert NO-OP into MEM2
+
+int hazardCheck(struct trace_item *IF1inst, struct trace_item *IF2inst, struct trace_item *IDinst, struct trace_item *EXinst, struct trace_item *MEM1inst, struct trace_item *MEM2inst, struct trace_item *WBinst ){
+  
+  // true if a load is followed by a any inst that could use 
+  //the register that is being loadedwhere the branch depends on
+  if (MEM1inst->type == ti_LOAD ){
+    if((EXinst->type == ti_ITYPE  || EXinst->type == ti_JRTYPE) && EXinst->sReg_a == MEM1inst->dReg)
+      return 2;
+    else if ((EXinst->type == ti_RTYPE  || EXinst->type == ti_STORE || EXinst->type == ti_BRANCH) && ( EXinst->sReg_b == MEM1inst->dReg || EXinst->sReg_a == MEM1inst->dReg))
+      return 2;
+  }
+  
+  // true if a load is followed by a any inst that could use 
+  //the register that is being loadedwhere the branch depends on
+  if (MEM2inst->type == ti_LOAD ){
+    if((EXinst->type == ti_ITYPE  || EXinst->type == ti_JRTYPE) && EXinst->sReg_a == MEM2inst->dReg)
+      return 3;
+    else if ((EXinst->type == ti_RTYPE || EXinst->type == ti_STORE || EXinst->type == ti_BRANCH) && (EXinst->sReg_b == MEM2inst->dReg || EXinst->sReg_a == MEM2inst->dReg))
+      return 3;
+  }
+  
+  // true if the WB instruction is a writing (type 1 or 2) and the ID instruction is a I 
+  //or R type and the destination register of WB matches either source register of ID
+  if (WBinst->type == ti_RTYPE || WBinst->type == ti_ITYPE){
+    if((IDinst->type == ti_ITYPE || IDinst->type ==ti_JRTYPE) && IDinst->sReg_a == WBinst->dReg)
+      return 1;
+    else if ((IDinst->type == ti_RTYPE  || IDinst->type == ti_STORE || IDinst->type== ti_BRANCH)&& (IDinst->sReg_a == WBinst->dReg || IDinst->sReg_b == WBinst->dReg)) 
+      return 1;
+  }
+    
+  
+  return 0;
+}
