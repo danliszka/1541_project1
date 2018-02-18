8acef55468699319ed846dd66e7a611fcf0478e4
willgs <31709696+willgs@users.noreply.github.com>
2018-02-18 15:04:35 -0500

    Update hazardCheck.c

--- hazardCheck.c
+++ hazardCheck.c
@@ -7,28 +7,28 @@ int hazardCheck(struct trace_item *IF1inst, struct trace_item *IF2inst, struct t
 	
 	// true if a load is followed by a any inst that could use 
 	//the register that is being loadedwhere the branch depends on
-	if (MEM1inst->type == 3 ){
-		if((EXinst->type == 2  || EXinst->type == 8) && EXinst->sReg_a == MEM1inst->dReg)
+	if (MEM1inst->type == ti_LOAD ){
+		if((EXinst->type == ti_ITYPE  || EXinst->type == ti_JRTYPE) && EXinst->sReg_a == MEM1inst->dReg)
 			return 2;
-		else if ((EXinst->type == 1  || EXinst->type == 4 || EXinst->type == 5) && ( EXinst->sReg_b == MEM1inst->dReg || EXinst->sReg_a == MEM1inst->dReg))
+		else if ((EXinst->type == ti_RTYPE  || EXinst->type == ti_STORE || EXinst->type == ti_BRANCH) && ( EXinst->sReg_b == MEM1inst->dReg || EXinst->sReg_a == MEM1inst->dReg))
 			return 2;
 	}
 	
 	// true if a load is followed by a any inst that could use 
 	//the register that is being loadedwhere the branch depends on
-	if (MEM2inst->type == 3 ){
-		if((EXinst->type == 2  || EXinst->type == 8) && EXinst->sReg_a == MEM2inst->dReg)
+	if (MEM2inst->type == ti_LOAD ){
+		if((EXinst->type == ti_ITYPE  || EXinst->type == ti_JRTYPE) && EXinst->sReg_a == MEM2inst->dReg)
 			return 3;
-		else if ((EXinst->type == 1  || EXinst->type == 4 || EXinst->type == 5) && (EXinst->sReg_b == MEM2inst->dReg || EXinst->sReg_a == MEM2inst->dReg))
+		else if ((EXinst->type == ti_RTYPE || EXinst->type == ti_STORE || EXinst->type == ti_BRANCH) && (EXinst->sReg_b == MEM2inst->dReg || EXinst->sReg_a == MEM2inst->dReg))
 			return 3;
 	}
 	
 	// true if the WB instruction is a writing (type 1 or 2) and the ID instruction is a I 
 	//or R type and the destination register of WB matches either source register of ID
-	if (WBinst->type == 1 || WBinst->type == 2){
-		if((IDinst->type == 2 || IDinst->type ==8) && IDinst->sReg_a == WBinst->dReg)
+	if (WBinst->type == ti_RTYPE || WBinst->type == ti_ITYPE){
+		if((IDinst->type == ti_ITYPE || IDinst->type ==ti_JRTYPE) && IDinst->sReg_a == WBinst->dReg)
 			return 1;
-		else if ((IDinst->type == 1  || IDinst->type == 4 || IDinst->type== 5)&& (IDinst->sReg_a == WBinst->dReg || IDinst->sReg_b == WBinst->dReg)) 
+		else if ((IDinst->type == ti_RTYPE  || IDinst->type == ti_STORE || IDinst->type== ti_BRANCH)&& (IDinst->sReg_a == WBinst->dReg || IDinst->sReg_b == WBinst->dReg)) 
 			return 1;
 	}
     
